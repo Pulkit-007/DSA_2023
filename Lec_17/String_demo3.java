1c2a05701bf289dee28a8630502367c5e5de0d21
Pulkit Sharma <pulkits94125@gmail.com>
2023-05-22 12:36:46 +0530

    Create String_demo3.java

---
+++ Lec_17/String_demo3.java
@@ -0,0 +1,10 @@
+package Lec_17;
+
+public class String_demo3 {
+    public static void main(String[] args) {
+        String s="hello";
+        System.out.println(10 + s + 30 + 40 );
+        System.out.println(10+30+40+s);
+        System.out.println(10+s+(30+40));
+    }
+}
