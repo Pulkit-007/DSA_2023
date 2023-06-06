7f64a2de90c50a76d07b82634320009051394e2f
Pulkit Sharma <pulkits94125@gmail.com>
2023-06-06 23:23:32 +0530

    Create Print_Dec.java

---
+++ Lec_21/Print_Dec.java
@@ -0,0 +1,17 @@
+package Lec_21;
+
+public class Print_Dec {
+    public static void main(String[] args) {
+        int n=5;
+        PD(n);
+    }
+    public static void PD(int n)
+    {
+        if(n==0)
+        {
+            return;
+        }
+        System.out.println(n);
+        PD(n-1);
+    }
+}
