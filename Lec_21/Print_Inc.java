4822e1aac6fa81a4692c692f12414e95cb9ccb8b
Pulkit Sharma <pulkits94125@gmail.com>
2023-06-06 23:34:55 +0530

    Create Print_Inc.java

---
+++ Lec_21/Print_Inc.java
@@ -0,0 +1,18 @@
+package Lec_21;
+
+public class Print_Inc {
+    public static void main(String[] args) {
+        int n=5;
+        PI(n);
+    }
+    public static void PI(int n)
+    {
+        if(n==0)
+        {
+            return;
+        }
+        PI(n-1);
+        System.out.println(n);
+
+    }
+}
