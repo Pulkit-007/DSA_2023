66667ab85298722cca01a89f1409972728bcd233
Pulkit Sharma <pulkits94125@gmail.com>
2023-07-25 22:01:41 +0530

    Backtracking-1

---
+++ Lec_27/Queen_Permutation.java
@@ -0,0 +1,29 @@
+package Lec_27;
+
+public class Queen_Permutation {
+    public static void main(String[] args) {
+        int n=4;//boxes
+        int tq=2;
+        boolean [] board=new boolean[n];//ek array le liya queen bethi hai ya nhi check karne ke liye by default false
+        Permutation(board,tq,0,"");
+
+    }
+    public static void Permutation(boolean [] board,int tq,int qpsf,String ans)
+    {
+        if(tq==qpsf)
+        {
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < board.length; i++) {
+            if(board[i]==false)
+            {
+                board[i]=true;
+                Permutation(board,tq,qpsf+1,ans+"b"+i+"q"+qpsf);
+                board[i]=false;//undo
+            }
+
+        }
+
+    }
+}
